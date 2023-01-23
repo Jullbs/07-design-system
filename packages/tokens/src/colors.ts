18d40d61c765f14ec39dedcbd08229ad3e0e8238
Juliana Daroz <julianadaroz@gmail.com>
2023-01-23 12:00:37 -0300

    Initial commit

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,25 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  blue100: '#458db3',
+  blue300: '#206b8a',
+
+  green100: '#466876',
+  green300: '#1b444b',
+  green500: '#091d1e',
+
+  pink100: '#cc5669',
+  pink200: '#8f4856',
+  pink300: '#71434d',
+  pink700: '#ff0078',
+}
